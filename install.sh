1f941424f84ce32c343a0b5a5be1f377dccd626a
Vitor Almeida <kassielalmeida@gmail.com>
2024-01-01 23:28:56 -0300

    add fix to discord black screen

--- install.sh
+++ install.sh
@@ -34,3 +34,22 @@ echo '{
   },
   "SKIP_HOST_UPDATE": true
 }' > ~/.config/discord/settings.json
+
+# prevent discord black screen on sharing
+sudo cp /etc/gdm/custom.conf /etc/gdm/custom.conf.backup
+sudo echo '# GDM configuration storage
+
+[daemon]
+AutomaticLoginEnable=False
+# Uncomment the line below to force the login screen to use Xorg
+#WaylandEnable=false
+
+[security]
+
+[xdmcp]
+
+[chooser]
+
+[debug]
+# Uncomment the line below to turn on debugging
+#Enable=true' > /etc/gdm/custom.conf
