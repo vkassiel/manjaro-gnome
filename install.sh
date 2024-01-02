c2cfbceb0472ffb78e8d0dfa7d72596d41f74452
Vitor Almeida <kassielalmeida@gmail.com>
2024-01-01 23:47:48 -0300

    adjust discord scripts

--- install.sh
+++ install.sh
@@ -18,22 +18,3 @@ done
 
 # change favorite apps on dock
 dconf write /org/gnome/shell/favorite-apps "['org.gnome.Nautilus.desktop', 'google-chrome.desktop', 'firefox.desktop', 'obsidian.desktop', 'org.gnome.Terminal.desktop', 'code.desktop', 'discord.desktop', 'com.slack.Slack.desktop', 'bitwarden.desktop', 'io.dbeaver.DBeaver.desktop', 'com.github.wwmm.easyeffects.desktop', 'filezilla.desktop', 'com.obsproject.Studio.desktop', 'gnome-system-monitor.desktop', 'rest.insomnia.Insomnia.desktop', 'postman.desktop', 'org.manjaro.pamac.manager.desktop', 'org.gnome.Extensions.desktop', 'com.mattjakeman.ExtensionManager.desktop', 'org.gnome.tweaks.desktop', 'org.gnome.Boxes.desktop']"
-
-# prevent discord black screen on sharing
-sudo cp /etc/gdm/custom.conf /etc/gdm/custom.conf.backup
-sudo echo '# GDM configuration storage
-
-[daemon]
-AutomaticLoginEnable=False
-# Uncomment the line below to force the login screen to use Xorg
-#WaylandEnable=false
-
-[security]
-
-[xdmcp]
-
-[chooser]
-
-[debug]
-# Uncomment the line below to turn on debugging
-#Enable=true' > /etc/gdm/custom.conf
--- manually/discord/fix-black-screen.sh
+++ manually/discord/fix-black-screen.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# IMPORTANT: just run this after starting discord app at least once
-# fix discord upgrade message popup (SKIP_HOST_UPDATE)
-# https://www.youtube.com/watch?v=3OEfr7L-gUk&t=302s
-echo '{
-  "IS_MAXIMIZED": false,
-  "IS_MINIMIZED": false,
-  "WINDOW_BOUNDS": {
-    "x": 240,
-    "y": 36,
-    "width": 1440,
-    "height": 1008
-  },
-  "SKIP_HOST_UPDATE": true
-}' > ~/.config/discord/settings.json
+# prevent discord black screen on sharing
+sudo cp /etc/gdm/custom.conf /etc/gdm/custom.conf.backup
+sudo echo '# GDM configuration storage
+
+[daemon]
+AutomaticLoginEnable=False
+# Uncomment the line below to force the login screen to use Xorg
+#WaylandEnable=false
+
+[security]
+
+[xdmcp]
+
+[chooser]
+
+[debug]
+# Uncomment the line below to turn on debugging
+#Enable=true' > /etc/gdm/custom.conf
