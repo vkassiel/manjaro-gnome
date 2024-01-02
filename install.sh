e87febfd630559ce024814d45607a877e097f2f2
Vitor Almeida <kassielalmeida@gmail.com>
2024-01-01 22:44:03 -0300

    add custom favorite apps on dock

--- install.sh
+++ install.sh
@@ -16,3 +16,7 @@ for folder in ${contexts[@]}; do
   sh main.sh
   cd -
 done
+
+# change favorite apps on dock
+favorite_apps="['org.gnome.Nautilus.desktop', 'google-chrome.desktop', 'firefox.desktop', 'obsidian.desktop', 'org.gnome.Terminal.desktop', 'code.desktop', 'discord.desktop', 'com.slack.Slack.desktop', 'bitwarden.desktop', 'io.dbeaver.DBeaver.desktop', 'com.github.wwmm.easyeffects.desktop', 'filezilla.desktop', 'com.obsproject.Studio.desktop', 'gnome-system-monitor.desktop', 'rest.insomnia.Insomnia.desktop', 'postman.desktop', 'org.manjaro.pamac.manager.desktop', 'org.gnome.Extensions.desktop', 'com.mattjakeman.ExtensionManager.desktop', 'org.gnome.tweaks.desktop', 'org.gnome.Boxes.desktop']"
+dconf write /org/gnome/shell/favorite-apps $favorite_apps
